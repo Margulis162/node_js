6ddd3c2a498d92a1bc5687fcd389dabfe7fc3a66
Margulis162 <markmargulis@proton.me>
2023-12-11 20:55:55 -0500

    simple event emitter try

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 // modules
- const counter = require('./counter.js')
- const stuff = require('./stuff.js')
+ const counter = require('./counter.js');
+ const stuff = require('./stuff.js');
+
+//  build in module which lets to do event listening
+ const events = require('events');
 // constants
+    const myEmitter = new events.EventEmitter();
     let time = 0;
     let  location = __dirname;
     // function expression
@@ -12,6 +16,10 @@
     function executor(fun){
         fun();
     }
+
+    myEmitter.on('someEvent', function(mssg){
+        console.log(mssg);
+    })
 
     //___ what's avaliable on global object
 
@@ -46,3 +54,4 @@ executor(four);
 console.log(counter(['strawberries', 'pickles', 'mangos']));
 console.log(stuff.pi);
 console.log(stuff.adder(2, 3));
+myEmitter.emit('someEvent', 'the event was emitted');
